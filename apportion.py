1db26357baa38ffdd2c040cad1f6cdd336eaa5d3
Eli Bradley <elibradley@utexas.edu>
2024-10-11 12:08:14 -0500

    Use p-queue

--- apportion.py
+++ apportion.py
@@ -1,12 +1,17 @@
 from math import sqrt
+from queue import PriorityQueue
 
-def apportion(value, weights):
-    length = len(weights)
-    apportionment = [1 for i in range(len(weights))]
+def apportion(house_size, entitlement):
+    length = len(entitlement)
+    apportionment = [1 for i in range(length)]
 
-    for i in range(value - length):
-        index = max(range(length),
-            key=lambda j: weights[j] / sqrt(apportionment[j] * (apportionment[j] + 1)))
-        apportionment[index] += 1
+    pqueue = PriorityQueue(maxsize=length)
+    for i in range(length):
+        pqueue.put((-entitlement[i] / sqrt(apportionment[i] * (apportionment[i] + 1)), i))
+
+    for i in range(house_size - length):
+        j = pqueue.get()[1]
+        apportionment[j] += 1
+        pqueue.put((-entitlement[j] / sqrt(apportionment[j] * (apportionment[j] + 1)), j))
 
     return apportionment
